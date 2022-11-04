2aba084a307e1945df06e445dc717ca7f1e7ef30
andmholm <andmholm@iu.edu>
2022-11-03 21:15:26 -0400

    basic map

--- maps/basic_map.py
+++ maps/basic_map.py
@@ -1,30 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def map_objects(dist, pos=(0, 0), width=100, height=100):
-    # set up grid and angle
-    grid = np.zeros((width, height, 3), dtype=np.uint8) + 211
-    offset = ((width - 1) // 2, (height - 1) // 2)
-    grid[pos[1] + offset[1], pos[0] + offset[0], 1:] = 0
-    theta = 360 / len(dist) 
-    place_point(grid, pos, (-1, 2), offset)
+class LocalMap:
 
-    for i, v in enumerate(dist):
-        # get x and y components of distances
-        rad = (theta * i * np.pi / 180)
-        
-        
-    return grid
+    def __init__(self, width, height, pos, rgb=(255, 0, 0)):
+        self.grid = np.zeros((width, height, 3), dtype=np.uint8) + 211
+        self.origin = (width - 1) // 2, (height - 1) // 2
+        self.size = (width, height)
+        self.pos = pos
+        self.rgb = rgb
+        self.objects = []
+        self.place_obj(pos, rgb, object=False)
 
-def place_point(grid, origin, point, offset):
-    i, j = origin[1] - point[1], origin[0] + point[0]
-    grid[i + offset[0], j + offset[1], :] = 0
+    def place_obj(self, pos, rgb=(0, 0, 0), object=True):
+        pos_ = convert(self.origin, pos)
+        self.grid[pos_[0], pos_[1], :] = rgb
+        if object:
+            self.objects.append((pos, rgb))
+
+    def get_objs(self):
+        return self.objects
+
+    def show(self, cmap="gray"):
+        plt.imshow(self.grid, cmap=cmap)
+        plt.show()
+
+    def __str__(self):
+        return str(self.grid.T)
+
+class GlobalMap:
+
+    def __init__(self, width, height, origin):
+        self.grid = np.zeros((width, height, 3), dtype=np.uint8) + 211
+        self.origin = origin
+        self.size = (width, height)
+        self.objects = []
+
+    def place_local_map(self, local_map, angle):
+        objs = local_map.get_objs()
+        for pos, rgb in objs:
+            pos_ = convert(self.origin, pos)
+            self.place_obj(pos_, rgb)
+
+    def place_obj(self, pos_, rgb=(0, 0, 0)):
+        self.grid[pos_[0], pos_[1], :] = rgb
+        self.objects.append((pos_, rgb))
+
+    def get_objs(self):
+        return self.objects
+
+    def show(self, cmap="gray"):
+        plt.imshow(self.grid, cmap=cmap)
+        plt.show()
+
+    def __str__(self):
+        return str(self.grid)
+
+
+def convert(origin, pos):
+    return origin[0] - pos[1], origin[1] + pos[0]
+
+def revert(origin, indices):
+    return indices[1] - origin[1], origin[0] - indices[0]
 
 if __name__ == "__main__":
-    low, high = 5, 10
-    beams = 10
-    dist = np.random.randint(low, high + 1, (beams, ))
-    grid = map_objects(dist, pos=(0, 0), width=20, height=20)
-    plt.imshow(grid, cmap="rainbow")
-    plt.show()
-
+    local_map = LocalMap(25, 25, (0, 0))
+    local_map.place_obj((-3, 3))
+    gloabl_map = GlobalMap(51, 51, (18, 18))
+    gloabl_map.place_local_map(local_map, None)
+    gloabl_map.show()
+    
