c5dc62fef155da045b8f51dd0b2a80d6c7389d3b
andmholm <andmholm@iu.edu>
2022-11-16 18:53:28 -0500

    updates need uncertainty

--- maps/basic_map.py
+++ maps/basic_map.py
@@ -6,53 +6,88 @@ import matplotlib.pyplot as plt
 
 class LocalMap:
     
-    def __init__(self, dim, nearest=False):
+    def __init__(self, dim, fov, nearest=False):
         if (dim % 2 == 0):
             raise ValueError("dim must be odd")
         self.dim = dim
         self.beam = dim // 2
         self.size = (dim, dim)
         self.center = (dim // 2, dim // 2)
+        self.fov = fov
         self.nearest = nearest
         self.grid = np.zeros((dim, dim, 3), dtype=int) + 211
         self.grid[self.center[1], self.center[0]] = (0, 255, 0) 
+        self.objects = set()
             
     def place_objs(self, objs):
         # (dist, angle)
         for dist, theta in objs:
             pos = calc_point(dist, theta)
-            if not self.inbounds(pos):
+            if not self.free(pos):
                 continue
-            self.place(pos)
+            self.place(pos, obj=True)
 
     # finds ray given a position
-    def raytrace(self, pos, delta=0.25):
-        grad = calc_grad((0, 0), pos)
+    def raytrace(self, dtheta=0.25):
+        
+        # locate objs in fov
+        for theta in np.arange(-self.fov // 2, self.fov // 2 + 1e-9, step=dtheta):
+            pos = calc_point(self.beam, theta)
+            grad = calc_grad((0, 0), pos)
+            self.trace(pos, grad)
+
+    def trace(self, pos, grad, delta=0.25):
+
+        beam = self.beam
+    
+        # vertical line
         if isinstance(grad, tuple):
-            for y in np.arange(0, self.beam + 1):
-                pos = (0, y)
-                if self.free(pos):
-                    self.place(pos, (255, 255, 255))
+            ystart = 1
+            if grad[0] < 0:
+                ystart *= -1
+                beam *= -1
+                delta *= -1
+            
+            for y in np.arange(ystart, beam, delta):
+                fpos = (0, y)
+                if self.free(fpos):
+                    self.place(fpos, rgb=(255, 255, 255))
                 else:
                     break
+
         else:
-            pass
+            xstart = 1
+            if pos[0] < 0:
+                xstart *= -1
+                beam *= -1
+                delta *= -1
+
+            for x in np.arange(xstart, beam, delta):
+                fpos = (x, grad * x)
+                if self.free(fpos):
+                    self.place(fpos, rgb=(255, 255, 255))
+                else:
+                    break
 
     # place pixel given (x, y) localization 
-    def place(self, pos, rgb=(0, 0, 0)):
+    def place(self, pos, rgb=(0, 0, 0), obj=False):
         if not self.inbounds(pos):
             raise ValueError(f"can't place pixel at {pos}")
-        if self.free(pos, rgb=(0, 255, 0)):
-            gpos = convert(self.center, pos, self.nearest)
+        # place the pixel/object
+        gpos = convert(self.center, pos, self.nearest)
+        if gpos not in self.objects:
             self.grid[gpos[0], gpos[1]] = rgb
+        if obj:
+            self.objects.add(gpos)
 
     # determines if pixel at (x, y) location is not an rgb color
     def free(self, pos, rgb=(0, 0, 0)):
         if not self.inbounds(pos):
-            raise ValueError(f"can't place pixel at {pos}")
+            return False
+        # see if on robot space or in occupied cell
         gpos = convert(self.center, pos, self.nearest)
         pixel = self.grid[gpos[0], gpos[1]].squeeze()
-        if tuple(pixel) == rgb:
+        if gpos == self.center or gpos in self.objects or tuple(pixel) == rgb:
             return False
         return True
 
@@ -97,7 +132,7 @@ def calc_point(dist, theta):
 
 # finds the gradient between two points
 def calc_grad(start, end):
-    if int(start[0]) == int(end[0]) == 0:
+    if start[0] == end[0] == 0:
         return (end[1],)
     return (end[1] - start[1]) / (end[0] - start[0])
 
@@ -118,11 +153,12 @@ def rotate_pos(pos, theta):
 
 
 if __name__ == "__main__":
-    local_map = LocalMap(11, nearest=False)
-    local_map.place((0.5, 0))
-    local_map.place((0, 5))
-    local_map.raytrace((0, 5))
+    local_map = LocalMap(101, 180, nearest=True)
+    objects = [(5, 0), (15, 21), (21, -90)]
+    local_map.place_objs(objects)
+    local_map.raytrace()
     local_map.view()
+        
     
     
     
